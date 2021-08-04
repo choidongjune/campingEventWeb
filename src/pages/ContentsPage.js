4dc74f256048610c71668ed090b95906df69dee3
nulspace <demian.choe@gmail.com>
2021-08-04 23:51:51 +0900

    2차 버전(담장자 반응)

--- src/pages/ContentsPage.js
+++ src/pages/ContentsPage.js
@@ -3,50 +3,57 @@ import { Container } from 'react-bootstrap';
 
 import familyImage from '../image/family.png';
 import ranking from '../image/ranking.png';
+const listStyle = {
+  listStylePosition : "inside",
+  textIndent: "-20px",
+  paddingLeft: "20px"
+}
 
 const ContentsPage = () => {
-  const [imageFlag, setFlag] = useState(true);
+  const [heightStyle, setHeightStyle] = useState({
+  height: "50vh",
+  alignItems: "center",
+})
   useEffect(()=>{
     let width = window.innerWidth;
     if(width<=765){
-      setFlag(false);
+      setHeightStyle({
+        height:"",
+        alignItems: "center",
+        marginBottom: "50px"
+      })
     }
-  })
+  },[])
   return(
     <Container>
-      <div className="row" style={{
-        height:"50vh",
-        alignItems: "center",
-      }}>
-        {imageFlag&&<div className="col-md-6">
+      <div className="row" style={heightStyle}>
+        <div className="col-md-6">
           <img src={familyImage} width="100%"/>
-        </div>}
+        </div>
         <div className="col-md-1"></div>
         <div className="col-md-5">
           <h4>참가방법</h4>
-          <li className="mb-1">응모 부분 : 2개 부문</li>
-          <li className="mb-1">사진부문 : 가족당 3장이내 원본 파일</li>
-          <li className="mb-1">동영상부문 : 가족당 2분이내 1영상 원본파일</li>
-          <li className="mb-1">응모기간 : 2021.08.09 ~ 2021.08.31</li>
-          <li className="mb-1">응모자격 : 대전광역시 거주자로써 2명이상으로 구성된 가족</li>
-          <li className="mb-1">접수된 작품은 일체 반환치 않으며, 초상권 및 저작권은 주최측에 귀속됨</li>
+          <li className="mb-1" style={listStyle}>응모 부분 : 2개 부문</li>
+          <li className="mb-1" style={listStyle}>사진부문 : 가족당 3장이내 원본 파일</li>
+          <li className="mb-1" style={listStyle}>동영상부문 : 가족당 2분이내 1영상 원본파일</li>
+          <li className="mb-1" style={listStyle}>응모기간 : 2021.08.09 ~ 2021.08.31</li>
+          <li className="mb-1" style={listStyle}>응모자격 : 대전광역시 거주자로써 2명이상으로 구성된 가족</li>
+          <li className="mb-1" style={listStyle}>접수된 작품은 일체 반환치 않으며, 초상권 및 저작권은 주최측에 귀속됨</li>
 
         </div>
       </div>
-      <div className="row" style={{
-        height:"50vh",
-        alignItems: "center",
-      }}>
+      <div className="row" style={heightStyle}>
+        <div className="col-md-6">
+          <img src={ranking} width="70%" style={{marginLeft: "15%"}}/>
+        </div>
         <div className="col-md-1"></div>
         <div className="col-md-5">
           <h4>시상내역</h4>
-          <li className="mb-1">15가족 (전체대상1, 부문별 금1, 은1, 동1, 입선 4)</li>
-          <li className="mb-1" style={{color: "#0f52ba", fontWeight: 700}}>관광상품권 / 대덕 e로움 마일리지 / 캠핑장 무료 이용권 등 시상품</li>
-          <li className="mb-1">입상작은 대덕구청, 대덕문화체육관 등 공공시설 활용하여 전시 또는 방영 예정</li>
+          <li className="mb-1" style={listStyle}>15가족 (전체대상1, 부문별 금1, 은1, 동1, 입선4)</li>
+          <li className="mb-1" style={{color: "#0f52ba", fontWeight: 700,listStylePosition : "inside", textIndent: "-25px", paddingLeft: "25px"}}>관광상품권 / 대덕 e로움 마일리지 / 캠핑장 무료 이용권 등 시상품</li>
+          <li className="mb-1" style={listStyle}>입상작은 대덕구청, 대덕문화체육관 등 공공시설 활용하여 전시 또는 방영 예정</li>
         </div>
-        {imageFlag&&<div className="col-md-6">
-          <img src={ranking} width="70%"/>
-        </div>}
+        
       </div>
     </Container>
   )
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import mainImage from '../image/mainPage.jpg';
+import campingImage from '../image/camping.jpg'
 const MainPage = () => {
   return(
     <div style={{
       height: "100vh",
-      backgroundImage: `url(${mainImage})`,
+      backgroundImage: `url(${campingImage})`,
       backgroundRepeat: "no-repeat",
       backgroundPosition:"center center",
       backgroundSize: "cover",
-      position:"relative"
+      position:"relative",
     }}>
       <div style={{
-        color: "white",
-        border: "5px solid white",
-        padding: "10px 0px",
+        color: "#fff",
+        border: "5px solid #fff",
+        borderRadius:"15px",
+        padding: "10px 5px",
         textAlign: "center",
         margin:"0 auto",
         top:"50%",
         left:"50%",
         transform: "translate(-50%,-50%)",
         position: "absolute",
+        backgroundColor: "#0f52ba",
         }}
         className="col-lg-3 col-10"><p className="h1">가족캠프, 위드 코로나19</p><p>A Family capming with COVID-19</p></div>
     </div>
